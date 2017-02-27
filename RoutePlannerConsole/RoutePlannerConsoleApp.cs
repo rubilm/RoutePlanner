f89ff327b164ca9bd5912244382e0888f0c0082a
Rubil Muriyamadom <rubil.muriyamadom@students.fhnw.ch>
2017-02-27 08:13:15 +0100

    Projektdateien hinzufügen.

---
+++ RoutePlannerConsole/RoutePlannerConsoleApp.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Reflection;
+using Fhnw.Ecnf.RoutePlanner.RoutePlannerLib;
+
+namespace Fhnw.Ecnf.RoutePlanner.RoutePlannerConsole
+{
+    class RoutePlannerConsoleApp
+    {
+       static void Main(string[] args)
+        {
+            string ver = Assembly.GetExecutingAssembly().GetName().Version.ToString();
+            Console.WriteLine($" Welcome to RoutePlanner ({ver})");
+            Console.ReadKey();
+
+            var wayPoint = new WayPoint("Windisch", 47.479319847061966, 8.212966918945312);
+            Console.WriteLine($"{wayPoint.Name}: {wayPoint.Latitude}/{wayPoint.Longitude}");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ RoutePlannerLib/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Allgemeine Informationen über eine Assembly werden über die folgenden 
+// Attribute gesteuert. Ändern Sie diese Attributwerte, um die Informationen zu ändern,
+// die einer Assembly zugeordnet sind.
+[assembly: AssemblyTitle("RoutePlannerLib")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("RoutePlannerLib")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Durch Festlegen von ComVisible auf "false" werden die Typen in dieser Assembly unsichtbar 
+// für COM-Komponenten.  Wenn Sie auf einen Typ in dieser Assembly von 
+// COM aus zugreifen müssen, sollten Sie das ComVisible-Attribut für diesen Typ auf "True" festlegen.
+[assembly: ComVisible(false)]
+
+// Die folgende GUID bestimmt die ID der Typbibliothek, wenn dieses Projekt für COM verfügbar gemacht wird
+[assembly: Guid("5c824021-f962-4d1a-bede-a9b289638514")]
+
+// Versionsinformationen für eine Assembly bestehen aus den folgenden vier Werten:
+//
+//      Hauptversion
+//      Nebenversion 
+//      Buildnummer
+//      Revision
+//
+// Sie können alle Werte angeben oder die standardmäßigen Build- und Revisionsnummern 
+// übernehmen, indem Sie "*" eingeben:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
